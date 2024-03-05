28412eb5682015ec963e74b269298328c54273cf
jalejotorresm <alejandrotorres5286@gmail.com>
2024-03-05 12:09:51 -0500

    New changes

--- src/components/hero_section.rs
+++ src/components/hero_section.rs
@@ -9,11 +9,19 @@ pub fn hero_section() -> Html {
                     <h1 class="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
                         { "Hola a todos, soy Alejandro" }
                     </h1>
-                    <p class="text-[#adb7be] text-lg lg:text-xl">
+                    <p class="text-[#adb7be] text-lg mb-6 lg:text-xl">
                         { "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ullam dignissimos dicta amet, doloribus nisi molestiae maiores numquam fuga eos consequuntur saepe eum, alias reprehenderit deleniti! Harum aliquam molestias non." }
                     </p>
+                    <div>
+                        <button></button>
+                        <button></button>
+                    </div>
                 </div>
-                <div class="col-span-5"></div>
+                <div class="col-span-5 place-self-center mt-4 lg:mt-0">
+                    <div class="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                        <img src="static/headshot.png" alt="imagen de perfil" class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" width="325" height="325"/>
+                    </div>
+                </div>
             </div>
         </section>
     }
